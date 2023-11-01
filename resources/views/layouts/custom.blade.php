f4e858d65ba58b9284c68254e8f09034359cd4cb
tjahajabarubsd <tjahajabarubsd@gmail.com>
2023-11-01 11:43:05 +0700

    Update meta viewport and navbar css

--- resources/views/layouts/custom.blade.php
+++ resources/views/layouts/custom.blade.php
@@ -20,7 +20,7 @@
   @endif
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, minimal-ui">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="title" content="@yield('title')" />
   <meta name="keywords" content="yamaha, sumbar, sumatera barat, padang, tjahaja baru, mio, mio j, soul gt, vixion, byson, minangkabau, scorpio, pameran yamaha, servis yamaha, motor yamaha">
   <meta name="description" content="Website Resmi Yamaha Sumatera Barat: CV. Tjahaja Baru. Official Website for Yamaha motor West Sumatra, Indonesia.">
