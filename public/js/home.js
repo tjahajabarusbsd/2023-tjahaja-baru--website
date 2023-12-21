1b09f46937e60f93138929e785e40d5ce3349169
tjahajabarubsd <tjahajabarubsd@gmail.com>
2023-12-21 08:43:31 +0700

    Update home js

--- public/js/home.js
+++ public/js/home.js
@@ -1,14 +1,22 @@
 $(document).ready(function () {
     $('.grid-container').slick({
-        dots: true,
+        dots: false,
         infinite: false,
         speed: 300,
-        slidesToShow: 4,
-        slidesToScroll: 4,
+        slidesToShow: 5,
+        slidesToScroll: 5,
         arrows: false,
         responsive: [
             {
-                breakpoint: 1024,
+                breakpoint: 1500,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 1100,
                 settings: {
                     slidesToShow: 3,
                     slidesToScroll: 3,
@@ -19,14 +27,16 @@ $(document).ready(function () {
                 breakpoint: 845,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToScroll: 2,
+                    dots: true
                 }
             },
             {
                 breakpoint: 568,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1
+                    slidesToScroll: 1,
+                    dots: true
                 }
             }
             // You can unslick at a given breakpoint now by adding:
