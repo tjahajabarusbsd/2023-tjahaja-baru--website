033964d92c73147f90590470077ba2f00b0b7cf8
tjahajabarubsd <tjahajabarubsd@gmail.com>
2024-02-01 15:21:48 +0700

    Update excelimportcontroller

--- app/Http/Controllers/ExcelImportController.php
+++ app/Http/Controllers/ExcelImportController.php
@@ -12,15 +12,22 @@ class ExcelImportController extends Controller
 {
     public function import(Request $request)
     {
-        $file = $request->file('file');
-
-        $nama_file = rand() . $file->getClientOriginalName();
-
-        $file->move('file_staff', $nama_file);
-
-        Excel::import(new XlsImport, public_path('/file_staff/' . $nama_file));
-
-        return redirect()->back()->with('success', 'Excel file imported successfully.');
+        $request->validate([
+            'file' => 'required|file|mimes:xls,xlsx|max:2048', // Sesuaikan dengan format file yang diizinkan dan batasan ukuran
+        ]);
+        
+        try {
+            $file = $request->file('file');
+            $nama_file = rand() . $file->getClientOriginalName();
+    
+            $file->move('file_staff', $nama_file);
+    
+            Excel::import(new XlsImport, public_path('/file_staff/' . $nama_file));
+    
+            return redirect()->back()->with('success', 'Excel file imported successfully.');
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Failed to import Excel file. ' . $e->getMessage());
+        }
     }
 
     public function importDealer(Request $request)
