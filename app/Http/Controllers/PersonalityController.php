751d77c9a673734fbbcd783ea232b8e9dc4d51fc
tjahajabarubsd <tjahajabarubsd@gmail.com>
2024-02-12 10:27:07 +0700

    Optimize code

--- app/Http/Controllers/PersonalityController.php
+++ app/Http/Controllers/PersonalityController.php
@@ -2,55 +2,38 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Group;
 use App\Models\Category;
+use App\Models\Group;
 use App\Models\PersonalityQuiz;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\View\View;
 
 class PersonalityController extends Controller
 {
     public function submitQuiz(Request $request)
     {   
-        // dd($request->result);
         $finalResult = $request->result;
-        
-        if($finalResult != ""){
-            $data = new PersonalityQuiz();
-            $data->result = $finalResult;
-            $data->save();
-        }
-        
-        // Proses data yang diterima dari permintaan AJAX
-        $xmlObject = simplexml_load_file(public_path('personality.xml'));
-        $features = $xmlObject->xpath("//result[motor='{$finalResult}']");
 
-       // Mengonversi objek SimpleXMLElement ke array
-        $featuresArray = json_decode(json_encode($features), true);
-
-        $category = Category::where('uri', $finalResult)->first();
-
-        if ( $category == null ) {
-            return view('errors/404');
-        } else {
-            $groups = Group::where('category_id', $category->id)->get();
+        if ($finalResult) {
+            PersonalityQuiz::create(['result' => $finalResult]);
         }
 
-        if (isset($groups)) {
-            // Render the Blade view with the necessary data
-            $html = View::make('quizResult', compact('groups'))->render();
-        } else {
-            $html = ''; // Or handle the case where $groups is not set
-        }
+        $featuresArray = Cache::remember('personality_features_' . $finalResult, 60, function () use ($finalResult) {
+            $xmlObject = simplexml_load_file(public_path('personality.xml'));
+            $features = $xmlObject->xpath("//result[motor='{$finalResult}']");
+            return json_decode(json_encode($features), true);
+        });        
 
-        // Menyusun respons yang ingin dikirim kembali ke klien
-        $response = [
+        $category = Category::where('uri', $finalResult)->firstOrFail();
+        $groups = $category->groups; // Eager loading
+
+        $html = view('quizResult', compact('groups'))->render();
+
+        return response()->json([
             'message' => 'Data berhasil diterima di controller.',
             'features' => $featuresArray,
             'html' => $html,
-        ];
-
-        // Mengirim respons dalam format JSON
-        return response()->json($response);
+        ]);
     }
 }
