4ac260cefa4a310e3d7a413c0ad549d5b7b9ddf3
tjahajabarubsd <tjahajabarubsd@gmail.com>
2024-03-21 10:44:11 +0700

    Update page user profile

--- resources/views/users/details.blade.php
+++ resources/views/users/details.blade.php
@@ -18,19 +18,37 @@
 @section('content')
 <div class="container">
     <div class="top-content">
-        {{-- <h1>Welcome!</h1> --}}
-        <div class="content-avatar">
-            <img src="{{ url('/images/dummy-image.png') }}" alt="">
+        <div id="dataProfile">
+            <div class="content-avatar">
+                <img src="{{ url('/images/dummy-image.png') }}" alt="">
+            </div>
+            <div class="content-title">
+                <p>{{ $user->name ?? 'Belum ada nama' }}</p>
+            </div>
+            
+            <div class="content-contact">
+                <p id="emailField">Email: {{ $user->email ?? '-' }}</p>
+                <p id="phoneField">No. HP: {{ $user->phone_number ?? '-' }}</p>
+            </div>
+
+            <button id="editProfileBtn" class="corner-button"><i class="fa-solid fa-pen-to-square"></i></button>
+
+            <a href="/logout" id="logoutBtn" class="corner-button"><i class="fas fa-sign-out-alt"></i></a>
         </div>
-        <div class="content-title">
-            <p>{{ $user->name ?? 'Belum ada nama' }}</p>
+
+        <div id="editProfileForm" >
+            <form id="profileForm" action="" method="POST">
+                @csrf
+                <label for="name">Nama:</label>
+                <input type="text" id="name" class="form-control" name="name" value="{{ $user->name ?? '' }}">
+                <label for="email">Email:</label>
+                <input type="email" id="email" class="form-control" name="email" value="{{ $user->email ?? '' }}">
+                <label for="phone_number">No. HP:</label>
+                <input type="text" id="phone_number" class="form-control" name="phone_number" value="{{ $user->phone_number ?? '' }}">
+                <button type="submit" class="btn btn-primary">Simpan</button>
+                <button type="button" id="cancelEditBtn" class="btn btn-primary">Cancel</button>
+            </form>
         </div>
-        
-        <div class="content-contact">
-            <p>Email: {{ $user->email ?? '-' }}</p>
-            <p>No. HP: {{ $user->phone_number ?? '-' }}</p>
-        </div>
-        
     </div>
     <div class="bottom-content">
         @if(isset($message))
@@ -63,132 +81,149 @@
             </div>
         @else
             <h1>Riwayat Servis</h1>
-            <div class="row">
-				<div class="col-md-12">
-					<div class="table-wrap">
-						<table class="table  table-hover" id="">
-                            <thead>
-                                <tr>
-                                <th>#</th>
-                                <th>Tanggal</th>
-                                <th>Invoice</th>
-                                <th>Kategori Servis</th>
-                                <th>Total Biaya</th>
-                                <th>&nbsp;</th>
-                                </tr>
-                            </thead>
-						    <tbody>
-                            @php
-                                $i = 1;
-                            @endphp
-                            @foreach($data as $item)
-                                <tr data-bs-toggle="collapse" data-bs-target="#collapse{{ $i }}" aria-expanded="@php if($i == 1) echo 'true'; else echo 'false'; @endphp" aria-controls="collapse{{ $i }}" @php if($i == 1) echo 'class'; else echo 'class="collapsed"'; @endphp>
-                                <th scope="row">{{ $i }}</th>
-                                <td>{{ date("Y-m-d H:i", strtotime($item['event_walkin'])) }}</td>
-                                <td>{{ $item['invoice'] }}</td>
-                                <td>{{ $item['svc_cat'] }}</td>
-                                <td>Rp. {{ number_format($item['cost_total'],0,",",".") }}</td>
-                                <td>
-                                    <i class="fa" aria-hidden="@php if($i == 1) echo 'true'; else echo 'false'; @endphp"></i>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td colspan="6" id="collapse{{ $i }}" class="accordion-collapse collapse acc @php if($i == 1) echo 'show'; else echo ''; @endphp" data-bs-parent="#accordion">
-                                        <div class="detail-wrapper">
-                                            <dl>
-                                                <dt>Tempat Servis</dt>
-                                                <dd>{{ $item['kode'] }}</dd>
-                                                <dt>Kategori Servis</dt>
-                                                <dd>{{ $item['svc_cat'] }}</dd>
-                                                <dt>Mekanik</dt>
-                                                <dd>{{ $item['mechanic_name'] }}</dd>
-                                                <dt>Unit</dt>
-                                                <dd>{{ $item['prod_nm'] }}</dd>
-                                            </dl>
-                                            <div class="row detail-cost">
-                                                <div class="col col-md-2">Paket Servis</div>
-                                                <div class="col col-md-5">
-                                                    @php 
-                                                        $svc_pac = json_decode($item['svc_pac']);
-                                                    @endphp
-                                                    @foreach( $svc_pac as $paket_servis)
+            @if (!empty($data))
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="table-wrap">
+                            <table class="table  table-hover" id="">
+                                <thead>
+                                    <tr>
+                                    <th>#</th>
+                                    <th>Tanggal</th>
+                                    <th>Invoice</th>
+                                    <th>Kategori Servis</th>
+                                    <th>Total Biaya</th>
+                                    <th>&nbsp;</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @php
+                                    $i = 1;
+                                @endphp
+                                @foreach($data as $item)
+                                    <tr data-bs-toggle="collapse" data-bs-target="#collapse{{ $i }}" aria-expanded="@php if($i == 1) echo 'true'; else echo 'false'; @endphp" aria-controls="collapse{{ $i }}" @php if($i == 1) echo 'class'; else echo 'class="collapsed"'; @endphp>
+                                    <th scope="row">{{ $i }}</th>
+                                    <td>{{ date("Y-m-d H:i", strtotime($item['event_walkin'])) }}</td>
+                                    <td>{{ $item['invoice'] }}</td>
+                                    <td>{{ $item['svc_cat'] }}</td>
+                                    <td>Rp. {{ number_format($item['cost_total'],0,",",".") }}</td>
+                                    <td>
+                                        <i class="fa" aria-hidden="@php if($i == 1) echo 'true'; else echo 'false'; @endphp"></i>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="6" id="collapse{{ $i }}" class="accordion-collapse collapse acc @php if($i == 1) echo 'show'; else echo ''; @endphp" data-bs-parent="#accordion">
+                                            <div class="detail-wrapper">
+                                                <dl>
+                                                    <dt>Tempat Servis</dt>
+                                                    <dd>{{ $item['kode'] }}</dd>
+                                                    <dt>Kategori Servis</dt>
+                                                    <dd>{{ $item['svc_cat'] }}</dd>
+                                                    <dt>Mekanik</dt>
+                                                    <dd>{{ $item['mechanic_name'] }}</dd>
+                                                    <dt>Unit</dt>
+                                                    <dd>{{ $item['prod_nm'] }}</dd>
+                                                </dl>
+                                                <div class="row detail-cost">
+                                                    <div class="col col-md-2">Paket Servis</div>
+                                                    <div class="col col-md-5">
+                                                        @php 
+                                                            $svc_pac = json_decode($item['svc_pac']);
+                                                        @endphp
+                                                        @foreach( $svc_pac as $paket_servis)
+                                                            <ul>
+                                                                <li>{{ $paket_servis }}</li>
+                                                            </ul>
+                                                        @endforeach
+                                                    </div>
+                                                    <div class="col"></div>
+                                                    <div class="col">
+                                                        @php 
+                                                            $svc_cost = json_decode($item['svc_cost']);
+                                                        @endphp
+                                                        @foreach( $svc_cost as $servis_cost)
+                                                            <ul>
+                                                                Rp. {{ number_format($servis_cost,0,",",".") }}
+                                                            </ul>
+                                                        @endforeach
+                                                    </div>
+                                                </div>
+                                                <div class="row detail-cost">
+                                                    <div class="col col-md-2">Part Terpakai</div>
+                                                    <div class="col col-md-5">
+                                                        @foreach( $item['part_name'] as $nama_part)
                                                         <ul>
-                                                            <li>{{ $paket_servis }}</li>
+                                                            <li>{{ $nama_part }}</li>
                                                         </ul>
-                                                    @endforeach
-                                                </div>
-                                                <div class="col"></div>
-                                                <div class="col">
-                                                    @php 
-                                                        $svc_cost = json_decode($item['svc_cost']);
-                                                    @endphp
-                                                    @foreach( $svc_cost as $servis_cost)
+                                                            
+                                                        @endforeach
+                                                    </div>
+                                                    <div class="col">
+                                                        @php 
+                                                            $part_qty = json_decode($item['part_qty']);
+                                                        @endphp
+                                                        @foreach ( $part_qty as $qty )
                                                         <ul>
-                                                            Rp. {{ number_format($servis_cost,0,",",".") }}
+                                                            {{ $qty }}
                                                         </ul>
-                                                    @endforeach
+                                                            
+                                                        @endforeach
+                                                    </div>
+                                                    <div class="col">
+                                                        @php 
+                                                            $part_cost = json_decode($item['part_cost']);
+                                                        @endphp
+                                                        @foreach ( $part_cost as $cost )
+                                                        <ul>
+                                                            Rp. {{ number_format($cost,0,",",".") }}
+                                                        </ul>
+                                                            
+                                                        @endforeach
+                                                    </div>
+                                                </div>
+                                                <div class="row detail-cost">
+                                                    <div class="col col-md-2">Total Biaya</div>
+                                                    <div class="col col-md-5"></div>
+                                                    <div class="col"></div>
+                                                    <div class="col">
+                                                        <ul>
+                                                            Rp. {{ number_format($item['cost_total'],0,",",".") }}
+                                                        </ul>
+                                                    </div>
                                                 </div>
                                             </div>
-                                            <div class="row detail-cost">
-                                                <div class="col col-md-2">Part Terpakai</div>
-                                                <div class="col col-md-5">
-                                                    @foreach( $item['part_name'] as $nama_part)
-                                                    <ul>
-                                                        <li>{{ $nama_part }}</li>
-                                                    </ul>
-                                                        
-                                                    @endforeach
-                                                </div>
-                                                <div class="col">
-                                                    @php 
-                                                        $part_qty = json_decode($item['part_qty']);
-                                                    @endphp
-                                                    @foreach ( $part_qty as $qty )
-                                                    <ul>
-                                                        {{ $qty }}
-                                                    </ul>
-                                                        
-                                                    @endforeach
-                                                </div>
-                                                <div class="col">
-                                                    @php 
-                                                        $part_cost = json_decode($item['part_cost']);
-                                                    @endphp
-                                                    @foreach ( $part_cost as $cost )
-                                                    <ul>
-                                                        Rp. {{ number_format($cost,0,",",".") }}
-                                                    </ul>
-                                                        
-                                                    @endforeach
-                                                </div>
-                                            </div>
-                                            <div class="row detail-cost">
-                                                <div class="col col-md-2">Total Biaya</div>
-                                                <div class="col col-md-5"></div>
-                                                <div class="col"></div>
-                                                <div class="col">
-                                                    <ul>
-                                                        Rp. {{ number_format($item['cost_total'],0,",",".") }}
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </td>
-                                </tr>
-                            @php
-                                $i++;
-                            @endphp
-						    @endforeach
-                            </tbody>
-						</table>
-					</div>
-				</div>
-			</div>
+                                        </td>
+                                    </tr>
+                                @php
+                                    $i++;
+                                @endphp
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            @else
+                <p>Belum ada data servis.</p>
+            @endif
         @endif
     </div>
 </div>
 @endsection
 
 @section('additional_script')
-    
+<script>
+    $(document).ready(function() {
+        $('#editProfileBtn').click(function() {
+            $('#editProfileForm').addClass('active');
+            $('#dataProfile').addClass('hide');
+            
+        });
+
+        $('#cancelEditBtn').click(function() {
+            $('#editProfileForm').removeClass('active');
+            $('#dataProfile').removeClass('hide');
+        });
+    });
+</script>
 @endsection
