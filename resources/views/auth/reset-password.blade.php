7f4d0ae148dcde819848c97c24f898f2cbc82d43
tjahajabarubsd <tjahajabarubsd@gmail.com>
2024-04-22 13:25:48 +0700

    Update reset password view

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -12,42 +12,55 @@
 
 @section('additional_css')
     <link rel="stylesheet" href="{{ asset('css/auth.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/main-form.css') }}" />
 @endsection
 
 @section('content')
 <div class="container">
     <div class="row">
-        <h3 class="text-center mb-4">Reset Password</h3>
+        <h1 class="text-center mb-4">Reset Password</h1>
 
         <form method="POST" action="{{ route('reset.password.update') }}" id="resetPasswordForm">
             @csrf
-
-            <input type="hidden" name="token" value="{{ $token }}">
-
-            <div class="form-group">
-                <label for="password">{{ __('Password Baru') }}</label>
-                
-                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required>
-                <div class="password-box">
-                    <span class="password-toggle-icon" id="togglePassword">
-                        <i class="fa fa-eye-slash"></i>
-                    </span>
+    
+            {{-- <input type="hidden" name="token" value="{{ $token }}"> --}}
+            
+            <div class="form-group row">
+                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password Baru') }}</label>
+    
+                <div class="col-md-6">
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" autofocus>
+                    <div class="password-box">
+                        <span class="password-toggle-icon" id="togglePassword">
+                            <i class="fa fa-eye-slash"></i>
+                        </span>
+                    </div>
+    
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                 </div>
-                
-                @error('password')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
             </div>
-
-            <div class="form-group">
-                <label for="password-confirm">{{ __('Konfirmasi Password Baru') }}</label>
-                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+    
+            <div class="form-group row">
+                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Konfirmasi Password Baru') }}</label>
+    
+                <div class="col-md-6">
+                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" autocomplete="new-password" required>
+                </div>
             </div>
-
-            <button type="submit" id="submitButton" class="btn btn-primary btn-block" onclick="submitForm()">{{ __('Reset Password') }}</button>
+    
+            <div class="form-group row mb-0">
+                <div class="col-md-6 offset-md-4">
+                    <button type="submit" id="submitButton" class="btn btn-primary" onclick="submitForm()">
+                        {{ __('Reset Password') }}
+                    </button>
+                </div>
+            </div>
         </form>
+        
     </div>
 </div>
 @endsection
