055db25658bd1735dbc02edc44f42e6376debebd
tjahajabarubsd <tjahajabarubsd@gmail.com>
2024-05-21 14:41:01 +0700

    Add class fw-bold

--- resources/views/users/simulasi-pinjaman-dana.blade.php
+++ resources/views/users/simulasi-pinjaman-dana.blade.php
@@ -1,5 +1,5 @@
 <div class="container-simulasi">
-    <h2 class="text-center mb-4">Kalkulator Simulasi Pinjaman</h2>
+    <h2 class="text-center mb-4 fw-bold">Kalkulator Simulasi Pinjaman</h2>
 
     <div class="form-group row">
         <label for="tipe" class="col-md-4 col-form-label text-md-right">Pilih tipe:</label>
