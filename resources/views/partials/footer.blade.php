43daf4f853ac9320be09f6723dde6ce9c3e46f61
tjahajabarubsd <tjahajabarubsd@gmail.com>
2024-07-09 10:30:42 +0700

    Update footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,18 +1,86 @@
 <footer>
     <section class="separator"></section>
-    <div class="footer-row first-row">
-    <div class="caption-first-row">Follow Us On</div>
-    <div class="sosmed">
-        <a href="https://www.facebook.com/tjahajabaru/" target="_blank"><img src="{{ url('/images/icons/fb.png')}}" alt=""></a>
-        <a href="https://www.tiktok.com/@tjahajabaru" target="_blank"><img src="{{ url('/images/icons/tiktok.png')}}" alt=""></a>
-        <a href="https://www.youtube.com/@YamahaSumbar" target="_blank"><img src="{{ url('/images/icons/yt.png')}}" alt=""></a>
-        <a href="https://www.instagram.com/tjahaja.baru/" target="_blank"><img src="{{ url('/images/icons/ig.png')}}" alt=""></a>
-    </div>
+    {{-- <div class="text-center first-row">
+        <div class="caption-first-row">Follow Us On</div>
+        <div class="sosmed">
+            <a href="https://www.facebook.com/tjahajabaru/" target="_blank"><img src="{{ url('/images/icons/fb.png')}}" alt=""></a>
+            <a href="https://www.tiktok.com/@tjahajabaru" target="_blank"><img src="{{ url('/images/icons/tiktok.png')}}" alt=""></a>
+            <a href="https://www.youtube.com/@YamahaSumbar" target="_blank"><img src="{{ url('/images/icons/yt.png')}}" alt=""></a>
+            <a href="https://www.instagram.com/tjahaja.baru/" target="_blank"><img src="{{ url('/images/icons/ig.png')}}" alt=""></a>
+        </div>
     </div>
     <div class="line"></div>
-    <div class="footer-row second-row">
-    <div class="caption-second-row">CV. TJAHAJA BARU | YAMAHA GROUP</div>
-    <div class="contact-second-row"><img src="{{ url('/images/icons/tlpn.png')}}" alt="">0811805898 <img src="{{ url('images/icons/mail.png')}}" alt="">sales.online@tjahaja-baru.com</div>
-    <div class="address-second-row"><img src="{{ url('images/icons/lokasi.png')}}" alt="">Jl. Damar No. 59 (25133) Padang - Sumatera Barat INDONESIA</div>
+    <div class="text-center second-row">
+        <div class="caption-second">CV. TJAHAJA BARU | YAMAHA GROUP</div>
+        <div class="contact-second-row"><img src="{{ url('/images/icons/tlpn.png')}}" alt="">0811805898 <img src="{{ url('images/icons/mail.png')}}" alt="">sales.online@tjahaja-baru.com</div>
+        <div class="address-second-row"><img src="{{ url('images/icons/lokasi.png')}}" alt="">Jl. Damar No. 59 (25133) Padang - Sumatera Barat INDONESIA</div>
+    </div> --}}
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5 mb-3 fw-semibold">
+                <a href="/">
+                    <img src="{{ url('/images/logo-footer.png')}}" title="Kembali ke Beranda" class="logo-image" alt="TJAHAJA BARU">
+                </a>
+                <div class="small mt-3">
+                    <div>CV. TJAHAJA BARU | YAMAHA GROUP</div>
+                    <div><img style="width: 18px;" src="{{ url('/images/icons/tlpn.png')}}" alt=""> 0811805898</div>
+                    <div><img style="width: 18px;" src="{{ url('images/icons/mail.png')}}" alt=""> sales.online@tjahaja-baru.com</div>
+                    <div><img style="width: 18px;" src="{{ url('images/icons/lokasi.png')}}" alt=""> Jl. Damar No. 59 (25133) Padang - Sumatera Barat INDONESIA</div>
+                </div>
+                
+            </div>
+            <div class="col-4 col-lg-2 mb-3">
+                <h5>Site Links</h5>
+                <ul class="list-unstyled">
+                    <li class="mb-2">
+                        <a href="/">Home</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="/profile">About Us</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="/consultation">Consultation</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="/contact">Contact Us</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="/dealers">Dealers</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="/compare">Compare</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-4 col-lg-2 mb-3">
+                <h5>Products</h5>
+                <ul class="list-unstyled">
+                    <li class="mb-2">
+                        <a href="/products/category/maxi">Maxi</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="/products/category/classy">Classy</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="/products/category/matic">Matic</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="/products/category/sport">Sport</a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="/products/category/moped">Moped</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-4 col-lg-3 mb-3">
+                <h5>Follow Us On</h5>
+                <div class="sosmed">
+                    <a href="https://www.facebook.com/tjahajabaru/" target="_blank"><img style="width: 33px;" src="{{ url('/images/icons/fb.png')}}" alt="tb-facebook"></a>
+                    <a href="https://www.tiktok.com/@tjahajabaru" target="_blank"><img style="width: 33px;" src="{{ url('/images/icons/tiktok.png')}}" alt="tb-tiktok"></a>
+                    <a href="https://www.youtube.com/@YamahaSumbar" target="_blank"><img style="width: 33px;" src="{{ url('/images/icons/yt.png')}}" alt="tb-youtube"></a>
+                    <a href="https://www.instagram.com/tjahaja.baru/" target="_blank"><img style="width: 33px;" src="{{ url('/images/icons/ig.png')}}" alt="tb-instagram"></a>
+                </div>
+            </div>
+        </div>
     </div>
 </footer>
