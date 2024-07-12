55de57b00a71faa1b68f89298c92cd9764141756
tjahajabarubsd <tjahajabarubsd@gmail.com>
2024-07-12 09:15:14 +0700

    Update home blade, css and js

--- public/js/home.js
+++ public/js/home.js
@@ -1,56 +1,34 @@
 $(document).ready(function () {
-    $('.grid-container').slick({
-        dots: false,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 5,
-        slidesToScroll: 5,
-        arrows: false,
-        responsive: [
-            {
-                breakpoint: 1500,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 4,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 1100,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 845,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 568,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    dots: true
-                }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-        ]
+    // $('#myModal').modal('show');
+
+    function closeModal() {
+        $('#myModal').modal('hide');
+        localStorage.removeItem('visited');
+    }
+
+    const swiperBanner = new Swiper('.banner-wrapper', {
+        slidesPerView: 1,
+        autoplay: {
+            delay: 3000,
+        },
+        loop: true,
+        pagination: {
+            el: '.banner-pagination',
+            clickable: true,
+        },
     });
 
-    $('.banner-wrapper').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 2000,
+    const swiperProductCategory = new Swiper('.grid-container', {
+        slidesPerView: 1,
+        breakpoints: {
+            640: { slidesPerView: 2 },
+            768: { slidesPerView: 3 },
+            1024: { slidesPerView: 4 },
+            1280: { slidesPerView: 5 },
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
     });
 });
