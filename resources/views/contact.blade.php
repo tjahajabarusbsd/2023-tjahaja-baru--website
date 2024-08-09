1260d29aed4c541d17dc59da352e21d12eca6a43
tjahajabarubsd <tjahajabarubsd@gmail.com>
2024-08-09 14:07:09 +0700

    Update contact.blade

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -49,70 +49,240 @@
                 <div class="main-form contact-form">
                     <div class="form-container">
                         <h1>Kirim Pesan</h1>
-                
-                        @if(Session::has('success'))
-                        <div class="alert alert-success">
-                            {{ Session::get('success') }}
-                            @php
-                                Session::forget('success');
-                            @endphp
-                        </div>
-                        @endif
-                
-                        <form action="{{ route('contact.submit') }}" method="post" class="myForm" onsubmit="disableButton()">
-                            @csrf
-                            @if(isset($_GET["dealer"]))
-                                <input name="dealer" type="text" hidden value="{{ $_GET["dealer"] }}">
-                            @endif
-                
-                            <div class="form-group" id="form-group">
+
+                        <form>
+                            <div class="form-group">
                                 <label for="name">Nama</label>
                                 <input name="name" class="form-control" id="name" type="text" value="{{ old('name') }}"  placeholder="Nama Lengkap" maxlength="50" required>
-                                @error('name')
-                                    <small>{{ $message }}</small>
-                                @enderror
                             </div>
                             
                             <div class="form-group">
                                 <label for="nohp">No Handphone (WhatsApp)</label>
                                 <input name="nohp" id="nohp" class="form-control" type="tel" value="{{ old('nohp') }}"  placeholder="08123456789" maxlength="15" required>
-                                @error('nohp')
-                                    <small>{{ $message }}</small>
-                                @enderror
                             </div>
-                            
+                        </form>
+                        
+                        <input name="url" type="text" hidden value="{{ Request::url() }}">
+
+                        <div class="form-group">
+                            <label>Pilihan</label><br>
+                            <label><input type="radio" name="option" value="pesan" required> Tulis Pesan</label><br>
+                            <label><input type="radio" name="option" value="motor" required> Motor yang Diinginkan</label><br>
+                            <label><input type="radio" name="option" value="dana" required> Pinjaman Dana Tunai</label>
+                        </div>
+
+                        <div id="optionPesan" style="display: none;" class="form-group">
                             <div class="form-group">
                                 <label for="message">Tulis Pesan</label>
-                                <textarea name="message" class="form-control" placeholder="Tulis pesan Anda disini" required>{{ old('message') }}</textarea>
-                                @error('message')
-                                    <small>{{ $message }}</small>
-                                @enderror
+                                <textarea id="tulis-pesan" name="message" class="form-control" placeholder="Tulis pesan Anda disini">{{ old('message') }}</textarea>
+                            </div>
+                            <div class="form-group">
+                                <button id="submit-pesan" class="btn btn-primary" onclick="disableButton()">Submit</button>
+                            </div>
+                        </div>
+
+                        <div id="optionMotor" style="display: none;" class="form-group">
+                            @if (!empty($value))
+                                <input name="sales" type="text" hidden value="{{ $value }}">
+                            @endif
+                            <div class="form-group">
+                                <label for="lists">Produk yang diminati</label>
+                                <select name="produk" id="pilih-produk" class="form-select" aria-label="Default select example">
+                                    <option selected disabled value=""> - pilih produk - </option>
+                                    @foreach ($lists as $list)
+                                        <option value="{{ $list->name }}">{{ $list->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            
+                            <div class="form-group" style="margin-top: 20px;">
+                                <label id="label-checkbox">
+                                    <input type="checkbox" name="terms" id="termsCheckbox">
+                                    Saya setuju bahwa informasi diatas mengizinkan TJAHAJA BARU untuk menghubungi Saya melalui telepon/WhatsApp.
+                                </label>
                             </div>
 
                             <div class="form-group">
-                                {!! RecaptchaV3::field('contact') !!}
-                                @error('g-recaptcha-response')
-                                <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-                                @enderror
+                                <button id="submit-motor" type="submit" class="btn btn-primary">Submit</button>
                             </div>
-                            
-                            <div class="form-group">
-                                <input id="submitButton" class="btn btn-primary" type="submit" value="Submit">
+                        </div>
+
+                        <div id="optionDana" style="display: none;" class="form-group">
+                            <div class="form-group row">
+                                <label for="tipe" class="col-md-4 col-form-label text-md-right">Pilih tipe:</label>
+                                <div class="col-md-6">
+                                    <select class="form-select" name="tipe" id="tipe">
+                                        <option selected disabled value=""> -- Pilih -- </option>
+                                        @foreach ($lists as $list)
+                                            <option value="{{ $list->name }}">{{ $list->name }}</option>
+                                        @endforeach
+                                        <option value="other"> -- Lainnya -- </option>
+                                    </select>
+                                    <span class="text-danger" id="error-tipe" style="display:none;">Tipe tidak boleh kosong.</span>
+                                </div>
                             </div>
-                        </form>
+
+                            <div id="input-tipe-lain" class="form-group row" style="display: none;">
+                                <label for="tipe-lain" class="col-md-4 col-form-label text-md-right">Masukkan tipe Yamaha lainnya:</label>
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control" id="tipe-lain" name="tipe-lain">
+                                    <span class="text-danger" id="error-tipe-lain" style="display:none;">Nama tipe tidak boleh kosong</span>
+                                </div>
+                            </div>
+
+                            @php
+                                $currentYear = date('Y'); 
+                                $startYear = 2000;
+                            @endphp
+                        
+                            <div class="form-group row">
+                                <label for="unit_tahun" class="col-md-4 col-form-label text-md-right">Pilih tahun:</label>
+                                <div class="col-md-6">
+                                    <select name="unit_tahun" id="unit_tahun" class="form-select">
+                                        <option selected disabled value=""> -- Pilih -- </option>
+                                        <?php for ($year = $startYear; $year <= $currentYear; $year++): ?>
+                                            <option value="<?= $year ?>"><?= $year ?></option>
+                                        <?php endfor; ?>
+                                    </select>
+                                    <span class="text-danger" id="error_unit_tahun" style="display:none;">Tahun tidak boleh kosong.</span>
+                                </div>
+                            </div>
+                        
+                            <div class="form-group row">
+                                <label for="harga_motor" class="col-md-4 col-form-label text-md-right">Estimasi Harga Motor:</label>
+                                <div class="col-md-6">
+                                    <input type="text" id="harga_motor" name="harga_motor" class="form-control">
+                                    <span class="text-danger" id="error_harga_motor" style="display:none;">Harga motor tidak boleh kosong.</span>
+                                </div>
+                            </div>
+                        
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button id="hitung" class="btn btn-primary">Hitung</button>
+                                </div>
+                            </div>
+                        
+                            <div id="hasil" style="display: none;"></div>                
+                        
+                            <div class="row">
+                                <div class="col-md-12" id="input_dana" style="display: none;">
+                                    <div class="form-group row">
+                                        <label class="col-md-4 col-form-label text-md-right">Dana yang Ingin Dicairkan (juta):</label>
+                                        <div class="col-md-6">
+                                            <label for="dana_dicairkan" id="dana_dicairkan_label" class="col-form-label text-md-right">dana</label>
+                                            <input type="range" id="dana_dicairkan" name="dana_dicairkan" class="" min="3000000" step="1000000" max="7000000" style="width: 100%;">
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="form-group row">
+                                        <label for="tenor" class="col-md-4 col-form-label text-md-right">Tenor (in months):</label>
+                                        <div class="col-md-6">
+                                            <select id="tenor" name="tenor" class="form-select">
+                                                <option selected value="11">11 bulan</option>
+                                                <option value="24">24 bulan</option>
+                                                <option value="35">35 bulan</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                        
+                                    <div class="form-group row mb-0">
+                                        <div class="col-md-6 offset-md-4">
+                                            <button id="hitung_angsuran" class="btn btn-primary">Hitung Angsuran</button>
+                                        </div>
+                                    </div>
+                                    
+                                    <div id="hasil-angsuran" style="display: none;">
+                                        <div class="break-line mt-5" style="
+                                            border-style: inset;
+                                            border-width: 1px;
+                                            border-color: #dadada;
+                                        "></div>
+                                        <div class="section-angsuran-result">
+                                            <div class="mt-3 col-md-8">
+                                                <div style="font-size: 25px; font-weight: 700">Estimasi Angsuran:</div>
+                                                <div style="font-size:10px;">*sudah termasuk bunga dan biaya admin</div>
+                                                <div style="margin: 5px 0; font-size: 25px; font-weight: 700">
+                                                    <input type="hidden" name="angsuran_monthly" id="angsuran-monthly" value="">
+                                                    <span id="biaya-angsuran">Rp -</span>
+                                                    <span>/bulan*</span>
+                                                </div>
+                                                <div style="font-size: 12px">*) Estimasi nilai pinjaman bukan merupakan persetujuan pinjaman dana, bersifat tidak mengikat, dan dapat disesuaikan berdasarkan penilaian lebih lanjut serta kebijakan BPR Tjahaja Baru.</div>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <button id="submit-dana" type="submit" class="btn btn-primary btn-ajukan">Ajukan Pinjaman</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            {!! RecaptchaV3::field('contact') !!}
+                            @error('g-recaptcha-response')
+                            <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+                        <div class="error-msg-wrapper" style="display: none">
+                            <div class="validation-error">
+                                <div class="side-bg-error" ></div>
+                                <img src="{{ url('/images/error.png') }}" alt="error">
+                                <ul></ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </section>
+<div class="overlay" id="overlay">
+    <div class="overlay__inner">
+        <div class="overlay__content"><span class="spinner"></span></div>
+    </div>
+</div>
+
+<div id="myModal" class="modal">
+    <div class="modal-dialog modal-confirm">
+		<div class="modal-content">
+			<div class="modal-header">
+				<div class="icon-box">
+					<i class="material-icons">close</i>
+				</div>
+                <h4 class="modal-title w-100">Success!</h4>	
+			</div>
+			<div class="modal-body">
+				<p class="text-center">Your booking has been confirmed. Check your email for detials.</p>
+			</div>
+		</div>
+	</div>
+</div>
 @endsection
 
 @section('additional_script')
     <script src="{{ asset('js/contact.js') }}"></script>
+    <script src="{{ asset('js/simulasi.js') }}"></script>
+    <script>const siteKey = '{{ env("RECAPTCHAV3_SITEKEY") }}';</script>
     <script>
         function disableButton() {
-            document.getElementById('submitButton').setAttribute('disabled', 'disabled');
+            // document.getElementById('submitButton').setAttribute('disabled', 'disabled');
+            $(".btn-primary").prop('disabled', true);
         }
+        $(document).ready(function() {
+            $('input[name="option"]').change(function() {
+                var selectedOption = $(this).val();
+
+                $('#optionPesan, #optionMotor, #optionDana').hide();
+
+                if (selectedOption === 'pesan') {
+                    $('#optionPesan').show();
+                } else if (selectedOption === 'motor') {
+                    $('#optionMotor').show();
+                } else if (selectedOption === 'dana') {
+                    $('#optionDana').show();
+                }
+            });
+        });
     </script>
 @endsection
