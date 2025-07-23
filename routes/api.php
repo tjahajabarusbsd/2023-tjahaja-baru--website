602a9037b1552e3ae5451105aecaa0082b85570a
tjahajabarubsd <tjahajabarubsd@gmail.com>
2025-07-23 13:47:43 +0700

    tampah api store booking servis

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\v1\ProductController;
 use App\Http\Controllers\Api\v1\AuthController;
+use App\Http\Controllers\Api\v1\BookingServiceController;
 use App\Http\Controllers\Api\v1\MyMotorController;
 use App\Http\Controllers\Api\v1\UserController;
 
@@ -39,4 +40,5 @@ Route::prefix('v1')->group(function () {
     Route::post('/resend-otp', [AuthController::class, 'resendOtp']);
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/reset-password', [AuthController::class, 'resetPassword']);
+    Route::post('/booking-servis', [BookingServiceController::class, 'store']);
 });
