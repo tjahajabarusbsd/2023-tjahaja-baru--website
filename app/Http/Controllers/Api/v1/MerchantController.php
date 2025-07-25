b7eb42ef149c7fade60b4ba41f6025c54f61bed4
tjahajabarubsd <tjahajabarubsd@gmail.com>
2025-07-25 14:48:13 +0700

    tambah api detail merchant

--- app/Http/Controllers/Api/v1/MerchantController.php
+++ app/Http/Controllers/Api/v1/MerchantController.php
@@ -56,4 +56,50 @@ class MerchantController extends Controller
             'data' => $merchants,
         ], 200);
     }
+
+    public function show($id): JsonResponse
+    {
+        // Contoh data dummy detail merchant
+        $merchantDetails = [
+            '1' => [
+                'id' => 1,
+                'name' => 'Nama Merchant 1',
+                'logo' => 'https://yourcdn.com/images/merchant1.png',
+                'info' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus.',
+                'location' => [
+                    [
+                        'place' => 'Sentral Yamaha Damar',
+                        'address' => 'Jl. Damar No. 59 (25117) Padang - Sumatera Barat INDONESIA'
+                    ]
+                ],
+                'promos' => [
+                    [
+                        'title' => 'Promo Kredit Diskon',
+                        'valid_until' => '25 Juli 2025'
+                    ],
+                    [
+                        'title' => 'Diskon 20rb',
+                        'valid_until' => '31 Des 2025'
+                    ]
+                ]
+            ],
+            // Kamu bisa tambah data lainnya di sini...
+        ];
+
+        if (!isset($merchantDetails[$id])) {
+            return response()->json([
+                'status' => 'error',
+                'code' => 404,
+                'message' => 'Merchant tidak ditemukan',
+                'data' => null,
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'code' => 200,
+            'message' => 'Detail merchant berhasil dimuat',
+            'data' => $merchantDetails[$id],
+        ], 200);
+    }
 }
