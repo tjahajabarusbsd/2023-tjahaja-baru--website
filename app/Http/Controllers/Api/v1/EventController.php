171521723b47286341b88cc236a06a97e6430743
tjahajabarubsd <tjahajabarubsd@gmail.com>
2025-07-26 11:45:20 +0700

    update hardcode fungsi show id event

--- app/Http/Controllers/Api/v1/EventController.php
+++ app/Http/Controllers/Api/v1/EventController.php
@@ -42,24 +42,39 @@ class EventController extends Controller
     }
     public function show($id): JsonResponse
     {
-        // For demonstration, returning a static event.
-        // In a real application, you would fetch the event from the database.
-        $event = [
-            'id' => $id,
-            'title' => 'Event ' . $id,
-            'subtitle' => 'Dp Mulai 1.5 Juta',
-            'description' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-            'image' => 'https://duckdumber.com/bannerEvent1.png',
-            'start_date' => '12/12/2025',
-            'end_date' => '31/12/2025',
-            'type' => 'online',
+        // Contoh data dummy untuk detail event
+        $eventDetails = [
+            '1' => [
+                'id' => 1,
+                'title' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                'timestamp' => '1 jam lalu',
+                'views' => 150,
+                'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus.\n\nMaecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar.",
+                'bannerImage' => 'https://duckdumber.com/banerEventDetail.png',
+                'promo' => [
+                    'headline' => 'Dp Mulai',
+                    'amount' => '1,5 Juta',
+                    'note' => '*S&K Berlaku',
+                    'cta' => 'Dapatkan Keuntungan Beli Motor Classy Bulan Ini!',
+                ],
+            ],
+            // Tambah data event lainnya jika perlu
         ];
+
+        if (!isset($eventDetails[$id])) {
+            return response()->json([
+                'status' => 'error',
+                'code' => 404,
+                'message' => 'Event tidak ditemukan',
+                'data' => null,
+            ], 404);
+        }
 
         return response()->json([
             'status' => 'success',
             'code' => 200,
-            'message' => "Detail event dengan ID {$id} berhasil diambil",
-            'data' => $event,
+            'message' => 'Data event berhasil dimuat',
+            'data' => $eventDetails[$id],
         ], 200);
     }
 }
