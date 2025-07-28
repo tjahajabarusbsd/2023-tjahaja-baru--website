4376ab6d1e7adbc63829533d8b8979a0fb4dd9e0
tjahajabarubsd <tjahajabarubsd@gmail.com>
2025-07-28 14:53:37 +0700

    update json

--- app/Http/Controllers/Api/v1/NotificationController.php
+++ app/Http/Controllers/Api/v1/NotificationController.php
@@ -11,34 +11,34 @@ class NotificationController extends Controller
     {
         $notifications = [
             [
-                "id" => 1,
-                "type" => "pembelian_motor",
+                "id" => (string) 1,
+                // "type" => "pembelian_motor",
                 "title" => "Pembelian Motor Berhasil",
                 "message" => "Pembelian motor Yamaha Aerox Anda telah berhasil dan sedang diproses.",
                 "time" => "2025-07-28T10:30:00Z",
                 "relative_time" => "5 menit lalu",
                 "isRead" => false,
-                "related_id" => 1234
+                // "related_id" => 1234
             ],
             [
-                "id" => 2,
-                "type" => "jadwal_servis",
+                "id" => (string) 2,
+                // "type" => "jadwal_servis",
                 "title" => "Pengingat Jadwal Servis",
                 "message" => "Motor Anda dijadwalkan servis pada 30 Juli 2025 pukul 10:00.",
                 "time" => "2025-07-27T09:00:00Z",
                 "relative_time" => "5 menit lalu",
                 "isRead" => false,
-                "related_id" => 5678
+                // "related_id" => 5678
             ],
             [
-                "id" => 3,
-                "type" => "promo",
+                "id" => (string) 3,
+                // "type" => "promo",
                 "title" => "Promo Servis Spesial",
                 "message" => "Dapatkan diskon 20% untuk servis motor bulan ini.",
                 "time" => "2025-07-25T08:15:00Z",
                 "relative_time" => "5 menit lalu",
                 "isRead" => true,
-                "related_id" => null
+                // "related_id" => null
             ]
         ];
 
