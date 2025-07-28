c2471c9edff56d7440d84687fad1a4a0b233a28e
tjahajabarubsd <tjahajabarubsd@gmail.com>
2025-07-28 15:27:53 +0700

    tambah api index activity

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\v1\ActivityController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\v1\ProductController;
@@ -49,4 +50,5 @@ Route::prefix('v1')->group(function () {
     Route::get('/events', [EventController::class, 'index']);
     Route::get('/events/{id}', [EventController::class, 'show']);
     Route::get('/notifications', [NotificationController::class, 'index']);
+    Route::get('/activity', [ActivityController::class, 'index']);
 });
