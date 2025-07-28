ae1ae76a8c827ab00e6261913c9c3fd8629581a5
tjahajabarubsd <tjahajabarubsd@gmail.com>
2025-07-28 15:27:53 +0700

    tambah api index activity

---
+++ app/Http/Controllers/Api/v1/ActivityController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ActivityController extends Controller
+{
+    public function index()
+    {
+        $activity = [
+            [
+                "id" => 1,
+                "type" => "redeem",
+                "title" => "Redeem",
+                "description" => "25% discount voucher",
+                "date" => "Today, 1 Dec 2025",
+                "points" => "-2500"
+            ],
+            [
+                "id" => 2,
+                "type" => "services",
+                "title" => "Servis telah dipesan",
+                "description" => "",
+                "date" => "28 November 2025",
+                "points" => ""
+            ],
+            [
+                "id" => 3,
+                "type" => "referral",
+                "title" => "Invite Friends",
+                "description" => "Referral code",
+                "date" => "Today, 1 Dec 2025",
+                "points" => "+500"
+            ],
+            [
+                "id" => 4,
+                "type" => "scan",
+                "title" => "Scan QR Code",
+                "description" => "QR code touring",
+                "date" => "Today, 1 Dec 2025",
+                "points" => "+500"
+            ]
+        ];
+
+        return response()->json([
+            "status" => "success",
+            "code" => 200,
+            "message" => "Riwayat aktivitas poin berhasil dimuat",
+            "data" => $activity
+        ]);
+    }
+}
