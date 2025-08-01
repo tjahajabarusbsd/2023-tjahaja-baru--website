2bd63973aba37baeee4ea8e5ea31a0cdef76a7ac
tjahajabarubsd <tjahajabarubsd@gmail.com>
2025-08-01 14:35:05 +0700

    buat helpers apiResponse

---
+++ app/Helpers/ApiResponse.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Helpers;
+
+class ApiResponse
+{
+    public static function success($message = 'Success', $data = null, $code = 200)
+    {
+        return response()->json([
+            'status' => 'success',
+            'code' => $code,
+            'message' => $message,
+            'data' => $data,
+        ], $code);
+    }
+
+    public static function error($message = 'Error', $code = 400, $data = null)
+    {
+        return response()->json([
+            'status' => 'error',
+            'code' => $code,
+            'message' => $message,
+            'data' => $data,
+        ], $code);
+    }
+}
