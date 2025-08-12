4c4ecf8f6882c8fbb9b8aec4cad3f59083a8b12e
tjahajabarubsd <tjahajabarubsd@gmail.com>
2025-08-12 14:23:39 +0700

    refactor BookingServiceController to use BookingServiceRequest for validation and add BookingServiceRequest class for improved request handling

--- app/Http/Requests/BookingServiceRequest.php
+++ app/Http/Requests/BookingServiceRequest.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Requests;
 
-class BookingServiseRequest extends BaseRequest
+class BookingServiceRequest extends BaseRequest
 {
     /**
      * Determine if the user is authorized to make this request.
