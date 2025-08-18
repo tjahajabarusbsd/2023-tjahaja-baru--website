30c412889fa872982a31fb1a13471a978a754faa
tjahajabarubsd <tjahajabarubsd@gmail.com>
2025-08-18 14:45:27 +0700

    feat(EventController): enhance event detail retrieval and increment view count

--- app/Http/Controllers/Api/v1/EventController.php
+++ app/Http/Controllers/Api/v1/EventController.php
@@ -38,39 +38,31 @@ class EventController extends Controller
 
     public function show($id): JsonResponse
     {
-        // Contoh data dummy untuk detail event
+        $event = Event::findOrFail($id);
+
+        $event->increment('views');
+
         $eventDetails = [
-            '1' => [
-                'id' => 1,
-                'title' => 'Event 1',
-                'timestamp' => '1 jam lalu',
-                'views' => 230,
-                'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus.\n\nMaecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar.",
-                'bannerImage' => 'https://duckdumber.com/banerEventDetail.png',
-                'promo' => [
-                    'headline' => 'Dp Mulai',
-                    'amount' => '1,5 Juta',
-                    'note' => '*S&K Berlaku',
-                    'cta' => 'Dapatkan Keuntungan Beli Motor Classy Bulan Ini!',
-                ],
-            ],
-            // Tambah data event lainnya jika perlu
+            'id' => (string) $event->id,
+            'title' => $event->name,
+            'timestamp' => $event->created_at
+                ? $event->created_at->diffForHumans()
+                : '',
+            'views' => $event->views ?? 0,
+            'description' => $event->description,
+            'image' => asset($event->image),
+            'start_date' => $event->start_date
+                ? $event->start_date->format('d/m/Y')
+                : '',
+            'end_date' => $event->end_date
+                ? $event->end_date->format('d/m/Y')
+                : '',
+            'type' => $event->type,
         ];
 
-        if (!isset($eventDetails[$id])) {
-            return response()->json([
-                'status' => 'error',
-                'code' => 404,
-                'message' => 'Event tidak ditemukan',
-                'data' => null,
-            ], 404);
-        }
-
-        return response()->json([
-            'status' => 'success',
-            'code' => 200,
-            'message' => 'Data event berhasil dimuat',
-            'data' => $eventDetails[$id],
-        ], 200);
+        return ApiResponse::success(
+            'Detail event berhasil diambil',
+            $eventDetails
+        );
     }
 }
