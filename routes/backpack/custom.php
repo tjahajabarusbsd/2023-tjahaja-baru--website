ba8de0c7c25332dbf35354f5be4b62e3adc1ea51
tjahajabarusbsd <tjahajabarubsd@gmail.com>
2025-08-26 09:26:43 +0700

    feat: update permission untuk akses tiap menu

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -9,30 +9,37 @@ use Illuminate\Support\Facades\Route;
 // Routes you generate using Backpack\Generators will be placed here.
 
 Route::group([
-    'prefix'     => config('backpack.base.route_prefix', 'admin'),
+    'prefix' => config('backpack.base.route_prefix', 'admin'),
     'middleware' => array_merge(
         (array) config('backpack.base.web_middleware', 'web'),
         (array) config('backpack.base.middleware_key', 'admin')
     ),
     // 'middleware' => ['web','can:can_manage_content'],
-    'namespace'  => 'App\Http\Controllers\Admin',
+    'namespace' => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
-    // Route::crud('tag', 'TagCrudController');
-    // Route::crud('article', 'ArticleCrudController');
-    Route::crud('category', 'CategoryCrudController');
-    Route::crud('group', 'GroupCrudController');
-    Route::crud('variant', 'VariantCrudController');
-    Route::crud('banner', 'BannerCrudController');
-    Route::crud('promo', 'PromoCrudController');
-    Route::crud('contact', 'ContactCrudController');
-    Route::crud('dealer', 'DealerCrudController');
-    Route::crud('review', 'ReviewCrudController');
-    Route::crud('group-product-spec', 'GroupProductSpecCrudController');
-    Route::crud('consultation', 'ConsultationCrudController');
-    Route::crud('nomor-rangka', 'NomorRangkaCrudController');
-    Route::crud('booking-service', 'BookingServiceCrudController');
-    Route::crud('event', 'EventCrudController');
-    Route::crud('order-motor', 'OrderMotorCrudController');
-    Route::crud('reward', 'RewardCrudController');
-    Route::crud('merchant', 'MerchantCrudController');
+    Route::group([
+        'middleware' => ['can:can_manage_menu_website'],
+    ], function () {
+        Route::crud('category', 'CategoryCrudController');
+        Route::crud('group', 'GroupCrudController');
+        Route::crud('variant', 'VariantCrudController');
+        Route::crud('banner', 'BannerCrudController');
+        Route::crud('promo', 'PromoCrudController');
+        Route::crud('contact', 'ContactCrudController');
+        Route::crud('dealer', 'DealerCrudController');
+        Route::crud('review', 'ReviewCrudController');
+        Route::crud('group-product-spec', 'GroupProductSpecCrudController');
+        Route::crud('consultation', 'ConsultationCrudController');
+    });
+
+    Route::group([
+        'middleware' => ['can:can_manage_menu_mobile'],
+    ], function () {
+        Route::crud('nomor-rangka', 'NomorRangkaCrudController');
+        Route::crud('booking-service', 'BookingServiceCrudController');
+        Route::crud('event', 'EventCrudController');
+        Route::crud('order-motor', 'OrderMotorCrudController');
+        Route::crud('merchant', 'MerchantCrudController');
+        Route::crud('reward', 'RewardCrudController');
+    });
 }); // this should be the absolute last line of this file
