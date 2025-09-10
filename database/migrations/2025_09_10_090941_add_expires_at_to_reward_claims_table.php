4c39711e252c5c7f9f623db62a239d27a19cb089
tjahajabarusbsd <tjahajabarubsd@gmail.com>
2025-09-10 10:14:05 +0700

    add expires_at column to reward_claims table

---
+++ database/migrations/2025_09_10_090941_add_expires_at_to_reward_claims_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reward_claims', function (Blueprint $table) {
+            $table->timestamp('expires_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('reward_claims', function (Blueprint $table) {
+            $table->dropColumn('expires_at');
+        });
+    }
+};
