1d9e2617a37dd73fd28de212ce624d4ad3c0e713
tjahajabarusbsd <tjahajabarubsd@gmail.com>
2025-09-10 10:14:11 +0700

    add discount_type and discount_value columns to rewards table

---
+++ database/migrations/2025_09_10_091526_add_discount_columns_to_rewards_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rewards', function (Blueprint $table) {
+            $table->string('discount_type')->nullable()->after('quantity');
+            $table->integer('discount_value')->nullable()->after('discount_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('rewards', function (Blueprint $table) {
+            $table->dropColumn(['discount_type', 'discount_value']);
+        });
+    }
+};
