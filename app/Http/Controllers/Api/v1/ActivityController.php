daab965c3af1baa87dab9c516a6a12469869afdd
tjahajabarusbsd <tjahajabarubsd@gmail.com>
2025-09-12 09:47:19 +0700

    update activitycontroller

--- app/Http/Controllers/Api/v1/ActivityController.php
+++ app/Http/Controllers/Api/v1/ActivityController.php
@@ -12,25 +12,29 @@ class ActivityController extends Controller
     {
         $user = auth()->user();
 
-        // Ambil log dari database untuk user ini
         $activityLogs = ActivityLog::where('user_public_id', $user->id)
             ->orderBy('activity_date', 'desc')
             ->get()
             ->map(function ($log) {
                 return [
-                    'id'          => $log->id,
-                    'type'        => $log->type,
-                    'title'       => $log->title,
+                    'id' => $log->id,
+                    'type' => $log->type,
+                    'title' => $log->title,
                     'description' => $log->description ?? '',
                     'date' => $log->activity_date
                         ? \Carbon\Carbon::parse($log->activity_date)->format('d M Y')
                         : null,
-                    'points'      => $log->points > 0
+                    'points' => $log->points > 0
                         ? '+' . $log->points
                         : (string) $log->points
                 ];
             });
 
-        return ApiResponse::success('Daftar aktivitas berhasil diambil.', $activityLogs);
+        return ApiResponse::success(
+            $activityLogs->isNotEmpty()
+            ? 'Daftar aktivitas berhasil diambil'
+            : 'Tidak ada aktivitas tersedia',
+            $activityLogs
+        );
     }
 }
