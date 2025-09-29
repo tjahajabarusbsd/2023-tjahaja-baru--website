e3b0b74188f6886ff200b2b5322e267d50936a00
tjahajabarusbsd <tjahajabarubsd@gmail.com>
2025-09-29 16:12:44 +0700

    alter phone number di table user public nullable

---
+++ database/migrations/2025_09_29_161026_alter_phone_number_nullable_in_user_publics_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_publics', function (Blueprint $table) {
+            $table->string('phone_number')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_publics', function (Blueprint $table) {
+            $table->string('phone_number')->nullable(false)->change();
+        });
+    }
+};
