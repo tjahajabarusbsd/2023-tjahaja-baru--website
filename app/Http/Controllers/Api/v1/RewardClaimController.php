bf40550b64b928dc73801a5be638c4c2dc62733f
tjahajabarusbsd <tjahajabarubsd@gmail.com>
2025-10-20 11:23:48 +0700

    Buat endpoint tampilkan detail reward

--- app/Http/Controllers/Api/v1/RewardClaimController.php
+++ app/Http/Controllers/Api/v1/RewardClaimController.php
@@ -31,4 +31,29 @@ class RewardClaimController extends Controller
 
         return ApiResponse::success('Daftar reward yang sudah diklaim', $formatted);
     }
+
+    public function show($id)
+    {
+        $user = auth()->user();
+        $userProfile = $user->profile;
+
+        $claim = RewardClaim::with('reward.merchant')
+            ->where('user_profile_id', $userProfile->id)
+            ->where('id', $id)
+            ->first();
+
+        if (!$claim) {
+            return ApiResponse::error('Detail hadiah tidak ditemukan', 404);
+        }
+
+        $data = [
+            'id' => (string) $claim->id,
+            'merchantName' => $claim->reward->merchant ? $claim->reward->merchant->title : null,
+            'promo' => $claim->reward->title,
+            'qrCode' => $claim->kode_voucher,
+            'instruction' => 'Lihatkan Kode QR ini ke kasir'
+        ];
+
+        return ApiResponse::success('Detail reward claim berhasil diambil', $data);
+    }
 }
