292a63f3fba758ca93f84a5eefabd512e8d36d56
tjahajabarusbsd <tjahajabarubsd@gmail.com>
2025-11-07 14:45:40 +0700

    feat: add last_otp_sent_at field to UserPublic model and migration

---
+++ database/migrations/2025_11_07_141828_add_last_otp_sent_at_to_user_publics_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_publics', function (Blueprint $table) {
+            $table->timestamp('last_otp_sent_at')->nullable()->after('otp_expires_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_publics', function (Blueprint $table) {
+            $table->dropColumn('last_otp_sent_at');
+        });
+    }
+};
