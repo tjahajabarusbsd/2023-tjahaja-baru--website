08278519a407284b4ba33f3c0946b58a27c4dd39
tjahajabarusbsd <tjahajabarubsd@gmail.com>
2025-11-08 11:05:38 +0700

    menambahkan validasi password dalam bahasa indonesia

---
+++ resources/lang/id/validation.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'password' => [
+        'letters' => 'Password harus mengandung setidaknya satu huruf.',
+        'mixed' => 'Password harus mengandung huruf besar dan kecil.',
+        'numbers' => 'Password harus mengandung setidaknya satu angka.',
+        'symbols' => 'Password harus mengandung setidaknya satu simbol.',
+        'uncompromised' => 'Password yang diberikan pernah muncul dalam kebocoran data.',
+    ],
+];
