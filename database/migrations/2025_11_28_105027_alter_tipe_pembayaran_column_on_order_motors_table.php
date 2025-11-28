fec8adfb6c9b8a59632950950537cf9137415869
tjahajabarusbsd <tjahajabarubsd@gmail.com>
2025-11-28 14:35:22 +0700

    ganti tipe kolom status table order motors

---
+++ database/migrations/2025_11_28_105027_alter_tipe_pembayaran_column_on_order_motors_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order_motors', function (Blueprint $table) {
+            $table->string('tipe_pembayaran')->default('kredit')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order_motors', function (Blueprint $table) {
+            $table->enum('tipe_pembayaran', ['kredit', 'cash'])
+                ->default('kredit')->change();
+        });
+    }
+};
