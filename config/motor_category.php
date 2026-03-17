40040ac3c4bd406cf24ad10243355320afb88d37
tjahajabarusbsd <tjahajabarubsd@gmail.com>
2026-03-17 15:16:51 +0700

    tambah kategori pada QR code dan perbarui logika pemindaian untuk validasi model motor

---
+++ config/motor_category.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+  'maxi' => ['xmax', 'nmax', 'aerox', 'lexi'],
+  'classy' => ['grand filano', 'fazzio', 'fino'],
+  'matic' => ['freego', 'gear', 'mio', 'x-ride'],
+  'sport' => ['xsr', 'r15', 'r25', 'mt-25', 'mt-15', 'vixion'],
+  'moped' => ['mx king', 'jupiter', 'vega'],
+];
