f52f9c19adaba5d81cca6ac80dc5e5451d545827
tjahajabarusbsd <tjahajabarubsd@gmail.com>
2026-03-24 16:03:50 +0700

    tambah event PromoCreated dan listener SendPromoNotification untuk mengirim notifikasi saat promo baru dibuat

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,6 +18,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        \App\Events\PromoCreated::class => [
+            \App\Listeners\SendPromoNotification::class,
+        ],
     ];
 
     /**
