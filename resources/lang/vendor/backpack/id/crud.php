1c6c780bdb366ccc860a05fa2e8a5f6b2e335cb5
tjahajabarusbsd <tjahajabarubsd@gmail.com>
2026-04-24 15:42:52 +0700

    Add Indonesian language translations for CRUD operations

---
+++ resources/lang/vendor/backpack/id/crud.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'apply' => 'Terapkan',
+    'today' => 'Hari Ini',
+    'yesterday' => 'Kemarin',
+    'last_7_days' => '7 Hari Terakhir',
+    'last_30_days' => '30 Hari Terakhir',
+    'this_month' => 'Bulan Ini',
+    'last_month' => 'Bulan Lalu',
+    'custom_range' => 'Rentang Kustom',
+];
