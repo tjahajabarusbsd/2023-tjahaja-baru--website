2d4b882a0e8a8230e46c2e3ba892dcd6db212ba3
tjahajabarusbsd <tjahajabarubsd@gmail.com>
2026-09-17 15:27:08 +0700

    Add logging for daily quota reached notification details

--- app/Listeners/SendDailyQuotaReachedNotification.php
+++ app/Listeners/SendDailyQuotaReachedNotification.php
@@ -4,6 +4,7 @@ namespace App\Listeners;
 
 use App\Events\DailyQuotaReached;
 use App\Services\Notification\FcmService;
+use Illuminate\Support\Facades\Log;
 
 class SendDailyQuotaReachedNotification
 {
@@ -11,10 +12,20 @@ class SendDailyQuotaReachedNotification
   {
     $qrCode = $event->qrCode;
 
+    Log::info('Kuota harian QR code tercapai, broadcast dikirim', [
+      'qrcode_id' => $qrCode->id,
+      'kode' => $qrCode->kode,
+      'nama_qrcode' => $qrCode->nama_qrcode,
+      'promo' => $qrCode->promo->name,
+      'merchant' => $qrCode->promo->merchant->title,
+      'max_penggunaan_harian' => $qrCode->max_penggunaan_harian,
+      'tanggal' => now()->toDateString(),
+    ]);
+
     app(FcmService::class)->sendToTopic(
       'promo',
       'Kuota Harian Penuh',
-      "Kode {$qrCode->nama_qrcode} sudah mencapai batas penggunaan hari ini. Coba lagi besok."
+      "Kode QR dari Promo {$qrCode->promo->name} sudah mencapai batas penggunaan hari ini. Coba lagi besok."
     );
   }
 }
